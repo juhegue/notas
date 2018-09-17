7b24d1c02d347a92ec798b1cb19bd1463305f13b
Juan Hevilla <j.hevilla@diagram.es>
2018-09-17 11:24:06 +0200

    Ajustes páginas

--- web/views/nota.py
+++ web/views/nota.py
@@ -120,6 +120,7 @@ class NotaDownloadZip(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
 <head>
     <meta charset="utf-8">
     <title>%s</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">    
 </head>
 <body>
     %s
