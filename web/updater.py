350d1577e45da7a8fc13c0b63030ae3ff30fc9a6
Juan Hevilla <j.hevilla@diagram.es>
2022-11-18 11:36:32 +0100

    bugfix:Iniciar sheduler

--- web/updater.py
+++ web/updater.py
@@ -94,10 +94,12 @@ def start():     # Añadir el start en apps.py
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(("127.0.0.1", 47200))
-    except socket.error:
-        logger.info("¡scheduler ya iniciado!, NADA QUE HACER")
-    else:
+
         logger.info("scheduler iniciado")
         scheduler = BackgroundScheduler()
         scheduler.add_job(procesa_eventos, 'interval', minutes=5)
         scheduler.start()
+
+    except socket.error:
+        logger.info("¡scheduler ya iniciado!, NADA QUE HACER")
+
